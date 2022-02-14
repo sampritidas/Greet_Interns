2253e20eaa0ba573e74f43f4d68046cd70d4c464
Sampriti Das <99536474+sampritidas@users.noreply.github.com>
2022-02-14 11:23:23 +0530

    Implemented Greet_interns.sh and main.sh
    Added greet_intern function
    Made main.sh executable
    Greeted Sampriti, Manisha and Dipti

---
+++ greet_interns.sh
@@ -0,0 +1,4 @@
+function greet_intern() {
+  local intern_to_greet=$1
+  echo "Welcome ${intern_to_greet}"
+}
---
+++ main.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+source interns.sh
+
+greet_intern "Sampriti"
+greet_intern "Manisha"
+greet_intern "Dipti"
